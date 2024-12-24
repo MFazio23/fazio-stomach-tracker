d0bf2c2138479850d8387b4e8b414a1a5aa5599e
Michael Fazio <mfazio23@gmail.com>
2024-12-23 18:03:17 -0600

    Add "todo" (ish) to the Calendar screen

--- src/screens/CalendarScreen.tsx
+++ src/screens/CalendarScreen.tsx
@@ -1,9 +1,15 @@
-import {Typography} from '@mui/material';
+import {Box, Typography} from '@mui/material';
 
 export function CalendarScreen() {
     return (
-        <div>
+        <Box>
             <Typography variant="h2">Calendar</Typography>
-        </div>
+            <Typography variant="body1">
+                This will eventually have a calendar with some kind of summary.
+                Or else I'll delete the tab, who can say?
+                Or maybe we can just have this show charts instead, that may be good.
+            </Typography>
+            <Typography variant="h1">🤷🏻‍♀️</Typography>
+        </Box>
     )
 }
