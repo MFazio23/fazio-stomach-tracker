bfd6bbd541a28815411a6c5bbb47c54cdf0f9783
Michael Fazio <mfazio23@gmail.com>
2024-12-26 09:38:20 -0600

    Adding in some auth logic - test this on the real site.

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -11,23 +11,52 @@ import {
     useColorScheme
 } from '@mui/material';
 import {Nightlight, WbSunny} from '@mui/icons-material';
+import {getRedirectResult, GoogleAuthProvider, signInWithPopup, signInWithRedirect, User} from 'firebase/auth';
+import {auth} from '../firebase';
+import {useState} from 'react';
 
 export interface SettingsScreenProps {
     currentUserId: string;
     setCurrentUserId: (userId: string) => void;
 }
 
-export function SettingsScreen({currentUserId, setCurrentUserId}: SettingsScreenProps) {
+const provider = new GoogleAuthProvider();
 
+export function SettingsScreen({currentUserId, setCurrentUserId}: SettingsScreenProps) {
+    const [user, setUser] = useState<User | null>(null);
     const {mode, setMode} = useColorScheme();
 
     const toggleColorMode = () => {
         setMode(mode === 'light' ? 'dark' : 'light');
     }
 
-    /*const signOutOfAccount = async () => {
+    const signIntoAccount = async (method: 'popup' | 'redirect') => {
+        try {
+            if (method === 'popup') {
+                await signInWithPopup(auth, provider).then((result) => {
+                    console.log("popup result", result)
+                    setUser(result.user)
+                });
+            } else {
+                await signInWithRedirect(auth, provider)
+                const result = await getRedirectResult(auth);
+                if (result) {
+                    console.log("redirect result", result)
+                    // This is the signed-in user
+                    const user = result.user;
+                    setUser(user)
+                } else {
+                    console.error("No user found after redirect")
+                }
+            }
+        } catch (e) {
+            console.error("Error signing in", method, e)
+        }
+    }
+
+    const signOutOfAccount = async () => {
         await auth.signOut()
-    }*/
+    }
 
     return (
         <Box>
@@ -58,7 +87,23 @@ export function SettingsScreen({currentUserId, setCurrentUserId}: SettingsScreen
                     </Card>
                 </Grid>
 
-                {/*<Grid>
+                <Grid>
+                    <Card sx={{width: 300, margin: 1}}>
+                        <CardHeader title="Sign in"/>
+                        <CardContent>
+                            <Typography variant={'body2'}>
+                                Current user: {user ? user.displayName : 'No user found'}
+                            </Typography>
+                            <Button variant="contained" onClick={() => signIntoAccount('popup')}>
+                                Sign in (popup)
+                            </Button>
+                            <Button variant="contained" onClick={() => signIntoAccount('redirect')}>
+                                Sign in (redirect)
+                            </Button>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid>
                     <Card sx={{width: 300, margin: 1}}>
                         <CardHeader title="Sign out"/>
                         <CardContent>
@@ -67,7 +112,7 @@ export function SettingsScreen({currentUserId, setCurrentUserId}: SettingsScreen
                             </Button>
                         </CardContent>
                     </Card>
-                </Grid>*/}
+                </Grid>
             </Grid>
             <Typography variant="subtitle2">
                 <Link href="https://www.flaticon.com/free-icons/toilet" title="toilet icons">Toilet icons created by
